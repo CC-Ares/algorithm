e151e69b45502a5d5cf4c9a376141756720618c5
Ares <240629417@qq.com>
2020-05-05 10:24:13 +0800

    排序算法

--- src/main/java/ares/cjc/algorithm/BucketSort.java
+++ src/main/java/ares/cjc/algorithm/BucketSort.java
@@ -1,4 +1,64 @@
 package ares.cjc.algorithm;
 
+import java.util.*;
+
+/**
+ * 桶排序
+ *
+ * 时间复杂度：O(n+m+n(logn-logm)）
+ * 空间复杂度：O（m+n）
+ */
 public class BucketSort {
+
+    public static double[] sort(double[] arr) {
+        //1. 找到数组最大值和最小值
+        double min = 0;
+        double max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        //2. 找到最大值与最小值的差
+        double d = max - min;
+
+        //2. 初始化桶
+        int bucketNum = arr.length;
+        ArrayList<LinkedList<Double>> bucketList = new ArrayList<>();
+        for (int i = 0; i < bucketNum; i++) {
+            bucketList.add(new LinkedList<>());
+        }
+
+        //3. 遍历原始数组，将每个元素放入桶中
+        for (int i = 0; i < arr.length; i++) {
+            int num = (int)((arr[i] - min) * (bucketNum - 1) / d);
+            bucketList.get(num).add(arr[i]);
+        }
+
+        //4. 对每个桶内进行排序
+        for(int i = 0; i < bucketList.size(); i++){
+            Collections.sort(bucketList.get(i));
+        }
+
+        //5. 输出全部元素
+        double[] sortedArray = new double[arr.length];
+        int index = 0;
+        for(LinkedList<Double> list : bucketList){
+            for(double element : list){
+                sortedArray[index] = element;
+                index++;
+            }
+        }
+        return sortedArray;
+    }
+
+    public static void main(String[] args) {
+        double[] array = new double[]{4.12,6.421,0.0023,3.0,2.123,8.122,4.12,10.09};
+        double[] sortedArray = sort(array);
+        System.out.println(Arrays.toString(sortedArray));
+    }
 }
--- src/main/java/ares/cjc/algorithm/Test.java
+++ src/main/java/ares/cjc/algorithm/Test.java
@@ -1,13 +1,17 @@
 package ares.cjc.algorithm;
 
 import ares.cjc.algorithm.group.Insert;
+import ares.cjc.algorithm.group.Merge;
 import ares.cjc.algorithm.group.Selection;
+import ares.cjc.algorithm.group.Shell;
 
 public class Test {
     public static void main(String[] args) {
         int[] array = {1,8,9,3,5,4,7,2,6};
 //        Selection.sort(array);
-        Insert.sort(array);
+//        Insert.sort(array);
+//        Shell.sort(array);
+        Merge.sort(array);
         System.out.println(Selection.isSort(array));
     }
 }
